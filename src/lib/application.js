fc06d2546d9340d9801b200e9d90a5328f7e165b
Sergei Gannochenko <sergei.gannochenko@zenjob.com>
2019-02-19 20:30:09 +0100

    stable

--- src/lib/application.js
+++ src/lib/application.js
@@ -6,107 +6,105 @@ import logger from './logger';
 import attachHomeAPI from '../api/home';
 
 export default class Application {
-    static async make() {
-        logger.info('Initializing the application');
+  static async make() {
+    logger.info('Initializing the application');
 
-        const instance = new this();
+    const instance = new this();
 
-        const app = express();
-        instance.attachErrorHandler(app);
+    const app = express();
+    instance.attachErrorHandler(app);
 
-        const hostname = await settings.get('network.hostname', 'localhost');
-        const port = await settings.get('network.port', 3010);
+    const hostname = process.env.HOST || 'localhost';
+    const port = process.env.PORT || 8500;
 
-        app.set('host', hostname);
-        app.set('port', port);
-        // // increase the default parse depth of a query string and disable allowPrototypes
-        // app.set('query parser', query => {
-        //   return qs.parse(query, { allowPrototypes: false, depth: 10 });
-        // });
+    app.set('host', hostname);
+    app.set('port', port);
+    // // increase the default parse depth of a query string and disable allowPrototypes
+    // app.set('query parser', query => {
+    //   return qs.parse(query, { allowPrototypes: false, depth: 10 });
+    // });
 
-        const corsSettings = await settings.get('network.cors');
-        const origins = _.isne(corsSettings)
-          ? corsSettings.split(',').map(x => x.trim())
-          : [];
-        if (_.iane(origins)) {
-            app.use(
-              cors({
-                  origin: (origin, cb) => {
-                      // allow requests with no origin, like mobile apps or curl requests
-                      if (!origin || _.contains(origins, origin)) {
-                          return cb(null, true);
-                      }
+    const corsSettings = process.env.CORS || '';
+    const origins = _.isne(corsSettings)
+      ? corsSettings.split(',').map(x => x.trim())
+      : [];
+    if (_.iane(origins)) {
+      app.use(
+        cors({
+          origin: (origin, cb) => {
+            // allow requests with no origin, like mobile apps or curl requests
+            if (!origin || _.contains(origins, origin)) {
+              return cb(null, true);
+            }
 
-                      return cb(new Error(), false); // todo: throw 403
-                  },
-              }),
-            );
-        }
-
-        app.use(helmet());
-        // // turn on JSON parser for REST services
-        app.use(express.json());
-        // // turn on URL-encoded parser for REST services
-        // app.use(
-        //   express.urlencoded({
-        //     extended: true,
-        //   }),
-        // );
-
-        // write the middleware here
-        attachHomeAPI(app);
-
-        instance._express = app;
-        instance._database = database;
-        instance._graphql = server;
-
-        logger.info('Application initialized');
-
-        return instance;
+            return cb(new Error(), false); // todo: throw 403
+          },
+        }),
+      );
     }
 
-    attachErrorHandler(app) {
-        // catching async unhandled rejections
-        process
-          .on('unhandledRejection', reason => {
-              logger.error('Unhandled rejection', reason);
-          })
-          .on('uncaughtException', err => {
-              logger.error('Uncaught exception', err);
-          });
+    app.use(helmet());
+    // // turn on JSON parser for REST services
+    app.use(express.json());
+    // // turn on URL-encoded parser for REST services
+    // app.use(
+    //   express.urlencoded({
+    //     extended: true,
+    //   }),
+    // );
 
-        // catching normal unhandled exceptions
-        app.use((err, req, res, next) => {
-            logger.error('Unhandled exception', err);
-            res.send('Nasty error'); // todo: explain here
-        });
+    // write the middleware here
+    attachHomeAPI(app);
+
+    instance._express = app;
+
+    logger.info('Application initialized');
+
+    return instance;
+  }
+
+  attachErrorHandler(app) {
+    // catching async unhandled rejections
+    process
+      .on('unhandledRejection', err => {
+        logger.error('Unhandled rejection', err);
+      })
+      .on('uncaughtException', err => {
+        logger.error('Uncaught exception', err);
+      });
+
+    // catching normal unhandled exceptions
+    app.use((err, req, res, next) => {
+      logger.error('Unhandled exception', err);
+      res.send('Nasty error'); // todo: explain here
+    });
+  }
+
+  async listen() {
+    const port = this._express.get('port');
+    const hostname = this._express.get('host');
+
+    return new Promise(resolve => {
+      this._server = this._express.listen({ port }, () => {
+        logger.info(`🚀 Server ready at http://${hostname}:${port}`, !__TEST__);
+        resolve();
+      });
+    });
+  }
+
+  async launch() {
+    await this.listen();
+  }
+
+  async shutdown() {
+    if (this._server) {
+      return new Promise(resolve => {
+        this._server.close(resolve);
+      });
     }
+  }
 
-    async listen() {
-        const port = this._express.get('port');
-        const hostname = this._express.get('host');
-
-        return new Promise(resolve => {
-            this._server = this._express.listen({ port }, () => {
-                logger.info(`🚀 Server ready at http://${hostname}:${port}`, !__TEST__);
-                resolve();
-            });
-        });
-    }
-
-    async launch() {
-        await this.listen();
-    }
-
-    async shutdown() {
-        if (this._server) {
-            return new Promise(resolve => {
-                this._server.close(resolve);
-            });
-        }
-    }
-
-    getExpress() {
-        return this._express;
-    }
+  getExpress() {
+    return this._express;
+  }
 }
--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,42 +1,43 @@
 module.exports = {
-    debug: (message) => {
-        if (__DEV__) {
-            console.log(message);
-            return;
-        }
-        console.log(JSON.stringify({ level: 'DEBUG', message }));
-    },
-    info: (message) => {
-        if (__DEV__) {
-            console.log(message);
-            return;
-        }
-        console.log(JSON.stringify({ level: 'INFO', message }));
-    },
-    warn: (message) => {
-        if (__DEV__) {
-            console.log(message);
-            return;
-        }
-        console.log(JSON.stringify({ level: 'WARNING', message }));
-    },
-    error: (message) => {
-        if (__DEV__) {
-            console.log(message);
-            return;
-        }
+  debug: message => {
+    if (__DEV__) {
+      console.log(message);
+      return;
+    }
+    console.log(JSON.stringify({ level: 'DEBUG', message }));
+  },
+  info: message => {
+    if (__DEV__) {
+      console.log(message);
+      return;
+    }
+    console.log(JSON.stringify({ level: 'INFO', message }));
+  },
+  warn: message => {
+    if (__DEV__) {
+      console.log(message);
+      return;
+    }
+    console.log(JSON.stringify({ level: 'WARNING', message }));
+  },
+  error: (message, error) => {
+    console.dir(message);
+    if (__DEV__) {
+      console.log(message);
+      console.dir(error);
+      return;
+    }
 
-        if (message instanceof Error) {
+    if (error instanceof Error) {
+      const stack = error.stack.split('\n');
+      stack.shift();
 
-            const stack = message.stack.split('\n');
-            stack.shift();
+      error = {
+        message: error.message, // ;)))
+        stack: stack.join('\n'),
+      };
+    }
 
-            message = {
-                message: message.message, // ;)))
-                stack: stack.join('\n'),
-            };
-        }
-
-        console.log(JSON.stringify({ level: 'ERROR', message }));
-    },
+    console.log(JSON.stringify({ level: 'ERROR', message, error }));
+  },
 };
